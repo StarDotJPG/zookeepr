3e429e076ca504465253fd06a877d74fa1d6d782
Jason Greenia <jason.greenia@gmail.com>
2022-03-20 18:10:20 -0700

    updated app listen to point to port variable

--- server.js
+++ server.js
@@ -130,6 +130,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
